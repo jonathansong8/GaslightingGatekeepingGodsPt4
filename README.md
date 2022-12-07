b0ca444a9437c0dc49082981644d91ca9e46a522
dliu <dliu30@stuy.edu>
2022-12-07 18:11:18 -0500

    add structure

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ArRESTed Development by GaslightingGatekeepingGodsPt4
+# Jonathan Song, Daniel Liu, Nicholas Tarsis, Kevin Xiao
+# Description
+
+The API’s we will use are the Google Maps API, Air Quality API, Water Quality API, and to some extent, the Country API. We will use the Google Maps API to create tables of relevant data. Once the user clicks on a country, a window will pop up in the corner of their screen for that country that displays relevant information specific to that country, like Air Quality and Water Quality. In order to fully customize the window, we will use the Country API to show data like flags and population.
+
+# Launch Codes
+ * Clone repository with ```git@github.com:jonathansong8/GaslightingGatekeepingGodsPt4.git```
+ * cd into the respiratory with ```cd GaslightingGatekeepingGodsPt4```
+ * Run  ```pip install -r requirements.txt ``` to install necessary packages
+ * Run ```cd app``` to enter app directory
+ * Run  ```python __init__.py ``` and access the localhost
---
+++ app/__init__.py
@@ -0,0 +1,55 @@
+from flask import Flask             #facilitate flask webserving
+from flask import render_template, request   #facilitate jinja templating
+from flask import session, redirect, url_for, make_response        #facilitate form submission
+import os
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+app.secret_key = os.urandom(32)
+
+username = "ads"
+password = "admin"
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return render_template('response.html',username = session['username'])
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    '''
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    '''
+    if request.method == 'POST':
+        userIn = request.form.get('username')
+        passIn = request.form.get('password')
+        print(userIn)
+        print(passIn)
+        if(userIn != username):
+            resp = make_response(render_template("error.html", msg = "wrong username"),404)
+            return resp
+        elif passIn != password:
+            resp = make_response(render_template("error.html", msg = "wrong password"),404)
+            return resp
+        else:
+            session['username'] = request.form['username']
+            resp = render_template('response.html',username = session['username'])
+            return resp
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
