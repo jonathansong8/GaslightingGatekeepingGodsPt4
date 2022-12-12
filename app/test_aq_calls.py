02d03f9f0cb40dd531de25d95ef7fda0fdb3a88e
dliu <dliu30@stuy.edu>
2022-12-12 13:09:48 -0500

    parse air

--- app/test_aq_calls.py
+++ app/test_aq_calls.py
@@ -1,5 +1,6 @@
 import requests
 
+'''
 url = "https://api.openaq.org/v2/locations?limit=1000&page=1&offset=0&sort=desc&radius=10000&country=US&order_by=lastUpdated&dumpRaw=false"
 
 headers = {"accept": "application/json"}
@@ -25,4 +26,31 @@ for gas in response["results"]:
     second[gas["description"]] = gas["preferredUnit"]
 print(dict)
 print(second)
+'''
 
+def process(url):
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    response = response.json()
+    response = response["results"]
+    return response
+
+all_countries = process("https://api.openaq.org/v2/countries?limit=200&page=1&offset=0&sort=asc&order_by=country")
+dict_countries = {}
+for country in all_countries:
+    code = country["code"]
+    name = country["name"]
+    dict_countries[code] = name
+countries = list(dict_countries.keys())
+
+projects = process("https://api.openaq.org/v2/projects")
+#print(projects[0])
+count = 0
+dict_gases = {}
+for entry in projects:
+    param = entry["parameters"][0]["displayName"] #the dict is the only thing inside the list
+    val = str(entry["parameters"][0]["average"]) + " " + entry["parameters"][0]["unit"]
+    dict_gases[param] = val
+    count += 1
+print(count)
+print(dict_gases)
