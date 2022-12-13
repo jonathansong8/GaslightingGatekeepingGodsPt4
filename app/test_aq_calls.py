9b27d8e94f36874c898a1e3dae951b83277640f7
dliu <dliu30@stuy.edu>
2022-12-13 09:22:55 -0500

    lookup function

--- app/test_aq_calls.py
+++ app/test_aq_calls.py
@@ -35,25 +35,33 @@ def process(url):
     response = response["results"]
     return response
 
+#Returns a Dict of Country Codes Along with their Respective Names
 all_countries = process("https://api.openaq.org/v2/countries?limit=200&page=1&offset=0&sort=asc&order_by=country")
 dict_countries = {}
 for country in all_countries:
     code = country["code"]
     name = country["name"]
     dict_countries[code] = name
+#Get Only the Keys, which are the country codes
 countries = list(dict_countries.keys())
 
+
+#returns parsed project (likely unusable atm)
+''''
 projects = process("https://api.openaq.org/v2/projects")
-#print(projects[0])
 count = 0
 dict_gases = {}
 for entry in projects:
+    print(entry["parameters"][0])
     param = entry["parameters"][0]["displayName"] #the dict is the only thing inside the list
     val = str(entry["parameters"][0]["average"]) + " " + entry["parameters"][0]["unit"]
     dict_gases[param] = val
     count += 1
+#print(count)
 #print(dict_gases)
+'''
 
+#get all locations available in the openaq api and their corresponding entry id (menu dropdown?) [name,country:unique id]
 x = process("https://api.openaq.org/v2/locations")
 dict_locations = {}
 for entry in x:
@@ -64,7 +72,21 @@ for entry in x:
     code = entry["id"]
     dict_locations[code] = name
 ids = list(dict_locations.keys())
+print(dict_locations)
 
+def lookup(id):
+    link = process("https://api.openaq.org/v2/latest/" + str(id) + "?location_id?limit=100&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&dumpRaw=false")
+    if link[0]["location"] is None:
+        return "Invalid Location"
+    else:    
+        name_location = link[0]["location"]
+    temp = link[0]["measurements"]
+    return temp
+   
+
+
+
+'''
 arr = []
 for id in ids:
     y = process("https://api.openaq.org/v2/latest/" + str(id) + "?location_id?limit=100&page=1&offset=0&sort=desc&radius=1000&order_by=lastUpdated&dumpRaw=false")
@@ -77,7 +99,4 @@ for id in ids:
         print(key)
         print(any["value"])
         arr.append([key,any["value"]])
-
-print(arr)
-
-
+'''
